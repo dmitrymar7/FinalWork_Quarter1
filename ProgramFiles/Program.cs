43862b30117940396faa93d294264c251357f5f7
Dima <markovda7@gmail.com>
2022-09-29 16:05:54 +0300

    add ProgramFiles

---
+++ ProgramFiles/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] inputArray = Console.ReadLine().Split(", ");
+
+int countArray  = 0;
+
+for (int i = 0; i < inputArray.Count(); i++){
+    if (inputArray[i].Length <= 3){
+        countArray++;
+    }
+}
+
+string[] resultArray = new String[countArray];
+int indexResultArray = 0;
+for (int i = 0; i < inputArray.Count(); i++){
+    if (inputArray[i].Length <= 3){
+        resultArray[indexResultArray] = inputArray[i];
+        indexResultArray++;
+    }    
+} 
+
+Console.WriteLine(String.Join(", ", resultArray));
